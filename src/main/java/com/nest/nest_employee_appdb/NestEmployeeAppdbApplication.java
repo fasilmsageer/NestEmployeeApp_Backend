eb77b1c6409f4e21b31aaab221a2b2d008885c13
fasilmsageer <fasilmsageer@gmail.com>
2022-12-11 23:11:15 +0530

    first

---
+++ src/main/java/com/nest/nest_employee_appdb/NestEmployeeAppdbApplication.java
@@ -0,0 +1,13 @@
+package com.nest.nest_employee_appdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestEmployeeAppdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestEmployeeAppdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nest/nest_employee_appdb/NestEmployeeAppdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.nest_employee_appdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NestEmployeeAppdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
